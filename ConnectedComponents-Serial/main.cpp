f3c88ba21390f1f74c424ef6413b1345f2387630
Matt Welch <mattw0308@gmail.com>
2012-02-11 17:53:25 -0700

    Main is reading graphs in of all sizes and crating an adjacency matrix representing the graph.

--- ConnectedComponents-Serial/main.cpp
+++ ConnectedComponents-Serial/main.cpp
@@ -31,13 +31,28 @@ using std::cin;
 #include <fstream>
 using std::ifstream;
 
+#define DEBUG
 
+int** graph;
+int numNodes;
+
+void printMatrix(int** graph, int numNodes){
+	cout << "Graph Adjacency Matrix::" << endl;
+	for(int i=1; i<numNodes+1; ++i){
+		//print adjacency matrix contents
+		for(int j = 1; j<numNodes+1; ++j){
+			cout << graph[i][j];
+		}
+		cout << endl;
+	}
+	cout << endl;
+}
 
 // beginning of Connected Components driver
 int main(){
 	ifstream infile;
 	string inFileName = "graph.txt";
-	int numNodes;
+	int neighbors;
 	string token;
 
 	// ifstream.open() reauires a const char* 
@@ -50,10 +65,40 @@ int main(){
 		// read the nodes in from the graph
 		infile >> numNodes; // read in the number of nodes from the first line
 
-		for(int i=0; i<numNodes; i++){
-			
+		// create adjacency matrix here (one more row than needed so can ignore row-0
+		graph = new int*[numNodes+1];
+		for(int i=0; i < numNodes+1; ++i){
+			graph[i] = new int[numNodes + 1];
+
+			//initialize graph to zeroes
+			for(int j = 0; j < numNodes+1; ++j){
+				graph[i][j] = 0;
+			}
+		}
+
+#ifdef DEBUG
+		cout << "Initialized::" << endl;
+		printMatrix(graph, numNodes);
+#endif
+
+		for(int i=1; i<=numNodes; ++i){
+			infile >> neighbors;
+
+#ifdef DISABLE
+			cout << neighbors << endl;
+#endif
+			for(int j=1; j <= neighbors; ++j){
+				infile >> token;
+				graph[i][atoi(token.c_str())] = 1;
+			}
+
 		}
 		
+#ifdef DEBUG
+		cout << "Filled::" << endl;
+		printMatrix(graph, numNodes);
+#endif
+
 		infile.close();
 		cout << endl <<"A graph has been read in from the file \"" << inFileName << "\"." << endl;
 	}
